a029fdc96874b4a4799c68aa3c66661f3a0d6325
LJNERO8 <ljnero8@gmail.com>
2022-12-26 05:13:48 +0200

    task 17 got pokemon names and weight list.

--- python-course-task17/main.py
+++ python-course-task17/main.py
@@ -6,9 +6,21 @@ BASE_URL = "https://pokeapi.co/api/v2/"
 
 
 async def get_pokemons(session):
-    result = await session.get(BASE_URL)
-    result_json= await result.json()
-    print(result_json)
+    start_time = time.time()
+    result = await session.get(f"{BASE_URL}/pokemon")
+    result_json = await result.json()
+    # print(result_json)
+    header = f"| {'Pokemon name:':30} | {'Pokemon weight':20} |"
+    header_separator = "-"*len(header)
+    print(header_separator)
+    print(header)
+    print(header_separator)
+    for i in result_json['results']:
+        result = await session.get(i['url'])
+        result_json = await result.json()
+        print(f"| {str(result_json['name']):30} | {str(result_json['weight']):20} |")
+    print(header_separator)
+    print(f"Time taken for processing the API requests: {time.time()-start_time}.")
 
 
 async def main():
